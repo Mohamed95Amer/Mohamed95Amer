0eec8d0e7a365b337126ca1c0f2dff36ef267a42
Mohamed95Amer <mohamed.m.amer95@gmail.com>
2026-08-13 15:06:44 +0400

    Unify Majal Property navigation shell

--- construction-erp/custom-addons/construction_ui/static/src/sidebar/majal_sidebar.js
+++ construction-erp/custom-addons/construction_ui/static/src/sidebar/majal_sidebar.js
@@ -58,6 +58,14 @@ export class MajalSidebar extends Component {
         return this.menuService.getCurrentApp();
     }
 
+    get isPropertyApp() {
+        const app = this.currentApp;
+        return !!app && (
+            app.name === "Majal Property" ||
+            (app.xmlid || "").startsWith("majal_property_ui.")
+        );
+    }
+
     /** The current app's menu, as a tree of sections and their children. */
     get sections() {
         const app = this.currentApp;
